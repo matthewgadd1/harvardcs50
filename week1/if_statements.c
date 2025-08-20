fe70b984376477d8b034dd16a2df6d6ad00cc7ab
Matthew Gadd <matthew.gadd@northernestateagencies.co.uk>
2025-08-20 15:36:57 +0100

    if statements

---
+++ week1/if_statements.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) 
+{
+    int x = get_int("What is x? ");
+    int y = get_int("What is y? ");
+
+    if (x < y) {
+        printf("X is less than Y\n");
+    }
+    else if (x > y) {
+        printf("X is not less than Y\n");
+    }
+    else {
+        printf("X is the same as Y\n");
+    }
+}
