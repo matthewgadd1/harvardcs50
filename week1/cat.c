b957408469df32d61420f21c2af0cd8ddce06b76
Matthew Gadd <matthew.gadd@northernestateagencies.co.uk>
2025-09-15 14:07:11 +0100

    function inheritance

--- week1/cat.c
+++ week1/cat.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <cs50.h>
 
-void meow(void);
+void meow(int n);
 
 int main(void)
 {
-    for (int i = 0; i < 3; i++)
-    {
-        meow();
-    }
+    int n = get_int("Number: ");
+    meow(n);
 }
 
-void meow(void)
+void meow(int n)
 {
-    printf("Meow\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("Meow\n");
+    }
 }
