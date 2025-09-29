3ff3c4215e10bfbc04d95b62e811c4609987f2c8
Matthew Gadd <matthew.gadd@northernestateagencies.co.uk>
2025-09-29 15:16:26 +0100

    adding an average function

--- week2/scores.c
+++ week2/scores.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <cs50.h>
 
+const int N = 3;
+
+float average(int length, int array[]);
+
 int main(void)
 {
-    int scores[3];
+    int scores[N];
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < N; i++)
     {
         scores[i] = get_int("Score: ");
     }
 
-    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
+    printf("Average: %f\n", average(N, scores));
+}
+
+float average(int length, int array[])
+{
+    int sum = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+    return sum / (float)length;
 }
