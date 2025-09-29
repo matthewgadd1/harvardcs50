ee56d8f316a140c6052a00e4fb0a2722990970af
Matthew Gadd <matthew.gadd@northernestateagencies.co.uk>
2025-09-29 14:06:15 +0100

    debugging

--- week2/buggy.c
+++ week2/buggy.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <cs50.h>
+
+void print_column(int h);
 
 int main(void)
 {
-    for (int i = 0; i < 3; i++)
+    int h = get_int("Height: ");
+    print_column(h);
+}
+
+void print_column(int h)
+{
+    for (int i = 0; i <= h; i++)
     {
-        printf("i is %i\n", i);
         printf("#\n");
     }
 }
