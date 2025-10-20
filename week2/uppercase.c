29fbc184576e2f1b57bbae2b42198923cd678286
Matthew Gadd <matthew.gadd@northernestateagencies.co.uk>
2025-10-20 15:27:44 +0100

    converting to uppercase using method

--- week2/uppercase.c
+++ week2/uppercase.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(void)
 {
@@ -8,14 +9,7 @@ int main(void)
     printf("After: ");
     for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if(s[i] >= 'a' && s[i] <= 'z')
-        {
-            printf("%c", s[i] - 32);
-        }
-        else
-        {
-            printf("%c", s[i]);
-        }
+        printf("%c", toupper(s[i]));
     }
     printf("\n");
 }
