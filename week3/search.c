64a73cbd0f6a855d0f0757d41ea272474e8d5bac
Matthew Gadd <matthew.gadd@northernestateagencies.co.uk>
2025-12-01 15:35:19 +0000

    adding linear search

--- week3/search.c
+++ week3/search.c
@@ -5,4 +5,13 @@ int main(void)
 {
     int numbers[] = {20, 500, 10, 5, 100, 1, 50};
 
+    int n = get_int("Number: ");
+    for(int i = 0; i < 7; i++)
+    {
+        if(numbers[i] == n)
+        {
+            printf("Found\n");
+        }
+    }
+    printf("Not found\n");
 }
